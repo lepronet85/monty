e4dd8610a1482d75a198814007832f85a54f14b0
lepronet85 <lepronet85@gmail.com>
2023-12-14 03:22:23 +0100

    Update files

--- additional_functions.c
+++ additional_functions.c
@@ -1,10 +1,5 @@
 #include "monty.h"
 
-void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
-
 /**
  * monty_rotl - Rotates the top value of a stack_t linked list to the bottom.
  * @stack: A pointer to the top mode node of a stack_t linked list.
